c1d929693d5a4006b8019d675fe08f64fd74d019
spriveen <rpeanuka@gmail.com>
2025-09-07 23:28:40 -0700

    update Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
   const [showLogin, setShowLogin] = React.useState(false)
@@ -10,6 +14,13 @@ const App = () => {
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-booking' element={<MyBookings/>}/>
+      </Routes>
     </>
   )
 }
