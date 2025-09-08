24b4873dd538d7f72531163c1add1d94844eed3c
spriveen <rpeanuka@gmail.com>
2025-09-08 00:11:00 -0700

    update hero.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
@@ -16,10 +16,10 @@ const App = () => {
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/car-details/:id' element={<CarDetails/>}/>
-        <Route path='/cars' element={<Cars/>}/>
-        <Route path='/my-booking' element={<MyBookings/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-booking' element={<MyBookings />} />
       </Routes>
     </>
   )
