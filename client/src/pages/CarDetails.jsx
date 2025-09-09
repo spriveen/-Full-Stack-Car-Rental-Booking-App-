6f31ae755cabf73d080952449b64ecd9865e652b
spriveen <rpeanuka@gmail.com>
2025-09-08 22:58:18 -0700

    update cardetails.jsx

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -7,6 +7,11 @@ const CarDetails = () => {
   const { id } = useParams()
   const navigate = useNavigate()
   const [car, setCar] = useState(null)
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const handleSubmit = async (e)=>{
+   e.preventDefault();
+  }
 
   useEffect(() => {
     setCar(dummyCarData.find(car => String(car._id) === String(id)))
@@ -87,7 +92,30 @@ const CarDetails = () => {
         </div>
 
         {/* Right: Booking form (you can build later) */}
-        <form></form>
+        <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6
+        text-gray-500'>
+       
+       <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency} {car.pricePerDay}<span className='text-base text-gray-400 font-norml'> per day</span></p>
+
+       <hr  className='border-borderColor my-6'/>
+
+       <div className='flex flex-col gap-2'>
+        <label  htmlFor='pickup date'>Pickup Date</label>
+        <input type='date'  className='border border-borderColor px-3 py-2 rounded-lg
+        ' required id='pickup-date' min={new Date().toISOString().split('T') [0]}/>
+       </div>
+
+       <div className='flex flex-col gap-2'>
+        <label  htmlFor='return date'>Return Date</label>
+        <input type='date'  className='border border-borderColor px-3 py-2 rounded-lg
+        ' required id='return-date' />
+       </div>
+
+       <button className='w-full bg-primary hovefr:bg-primary-dull
+       transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>Book Now</button>
+
+       <p className='text-center text-sm'>No Credit card required to reserve</p>
+        </form>
       </div>
     </div>
   ) : (
