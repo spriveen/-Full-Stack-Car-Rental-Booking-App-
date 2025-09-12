afc6958eeb24dde02c93157c36cfd81cacfd6e87
spriveen <rpeanuka@gmail.com>
2025-09-12 02:14:17 -0700

    update server js and db js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js"; // <-- add .js
+
+// Initializing Express App
+const app = express();
+
+// Connect DATABASE
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is running"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
