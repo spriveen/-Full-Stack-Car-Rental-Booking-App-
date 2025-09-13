c0a8e5e76f1cd560e0b57a7a47f1534d6adf5d17
spriveen <rpeanuka@gmail.com>
2025-09-12 23:08:48 -0700

    update token

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js"; // <-- add .js
+import userRouter from "./routes/userRoutes.js";
 
 // Initializing Express App
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Server is running"));
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
